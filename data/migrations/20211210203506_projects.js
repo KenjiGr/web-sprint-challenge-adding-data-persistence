812446fa7e4cb76ae801da00dd6396ebd4586707
Kenji Greene <kenjigrr@gmail.com>
2021-12-10 16:55:50 -0600

    created project resources table

--- data/migrations/20211210203506_projects.js
+++ data/migrations/20211210203506_projects.js
@@ -24,6 +24,23 @@ exports.up = async function(knex) {
                 .onDelete('RESTRICT')
                 .onUpdate('RESTRICT')
         })
+        .createTable('project_resources', (table) => {
+            table.increments('project_resource_id')
+            table.integer('project_id')
+                .notNullable()
+                .unsigned()
+                .references('project_id')
+                .inTable('projects')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+            table.integer('resource_id')
+                .notNullable()
+                .unsigned()
+                .references('resource_id')
+                .inTable('resources')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+        })
 };
 
 exports.down = async function(knex) {
