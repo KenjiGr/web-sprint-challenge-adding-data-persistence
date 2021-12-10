3d46b7755d85a79a6dca97b5903792e65d3ddd45
Kenji Greene <kenjigrr@gmail.com>
2021-12-10 14:24:55 -0600

    index and server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,7 @@
 // build your server here and require it from index.js
+const express = require('express');
+const server = express();
+const router = require('./project/router');
+
+server.use(express.json());
+server.use('/api/project', router);
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // start your server here
+const server = require('./api/server');
+const port = process.env.PORT || 7000;
+
+server.listen(port, () => {
+    console.log(`now listening on port ${port}`);
+})
