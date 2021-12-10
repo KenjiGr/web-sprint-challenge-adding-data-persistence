9fa266c31320e5baec59517000b3b1cdd0f4105f
Kenji Greene <kenjigrr@gmail.com>
2021-12-10 14:36:10 -0600

    initialized migration a framework for routers

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,11 @@
 // build your `/api/resources` router here
+const express = require('express');
+const router = express.Router();
+
+// `[POST] /api/resources` - Example of response body: {"resource_id":1,"resource_name":"foo","resource_description":null}
+router.use();
+
+// `[GET] /api/resources` Example of response body: [{"resource_id":1,"resource_name":"foo","resource_description":null}]
+router.use();
+
+module.exports = router
